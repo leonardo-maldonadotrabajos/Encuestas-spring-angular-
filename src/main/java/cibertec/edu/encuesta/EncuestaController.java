0982089efc32c020f0eb1cbcd7189b9f2ab73245
leonardo-maldonadotrabajos <maldonadocanazas2003@gmail.com>
2026-06-18 19:54:06 -0500

    Add CORS support to EncuestaController

--- src/main/java/cibertec/edu/encuesta/EncuestaController.java
+++ src/main/java/cibertec/edu/encuesta/EncuestaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "https://sistema-encuestas-frontend.onrender.com")
 @RestController
 @RequestMapping("/api/encuestas")
 public class EncuestaController {
